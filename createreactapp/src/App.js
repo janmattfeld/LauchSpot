29d79320879274a24d278e471d8dfd9691e2946f
frederike-ramin <frederike.ramin@student.hpi.de>
2017-07-09 11:58:54 +0200

    Style cards

--- createreactapp/src/App.js
+++ createreactapp/src/App.js
@@ -10,12 +10,12 @@ const Card = belle.Card;
 class App extends Component {
   render() {
       const picArray = [p1,p2,p3];
-      console.log("Array", picArray);
+      const cardStyle= {width: '40%', padding: '0px 0', boxShadow: '0px', margin: '0px', display: 'inline'};
       return (
       <div className="App">
           <TextInput placeholder="Insert your favourite album here"/>
           {picArray.map((pic) => {
-              return (<Card>
+              return (<Card style={cardStyle}>
                   <img src={pic}/>
               </Card>);
           })}
