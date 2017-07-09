e583d284ccc090ce136a7d76d674c3b106e0e771
frederike-ramin <frederike.ramin@student.hpi.de>
2017-07-09 12:56:18 +0200

    dirty beginning

--- createreactapp/src/App.js
+++ createreactapp/src/App.js
@@ -7,16 +7,35 @@ const belle = require('belle');
 const TextInput = belle.TextInput;
 const Card = belle.Card;
 
+class Logic {
+
+}
 class App extends Component {
+    constructor() {
+        this.state = {
+            albumPics: []
+        };
+    }
+
+    searchSpotify(searchString, callback) {
+        const picArray = [p1,p2,p3];
+        const response = {
+            se
+        }
+        callback.apply(picArray);
+    }
+
+    searchCallback() {
+        this.setState
+    }
   render() {
-      const picArray = [p1,p2,p3];
-      const cardStyle= {width: '40%', padding: '0px 0', boxShadow: '0px', margin: '0px', display: 'inline'};
+      const cardStyle= {width: '100%', padding: '0px 0', boxShadow: '0px', margin: '0px', display: 'inline'};
       return (
       <div className="App">
-          <TextInput placeholder="Insert your favourite album here"/>
-          {picArray.map((pic) => {
+          <TextInput placeholder="Insert your favourite album here" onclick={}/>
+          {picArray.map((pic, index) => {
               return (<Card style={cardStyle}>
-                  <img src={pic}/>
+                  <img src={pic} alt={"album_" + index} width='auto' display="block"/>
               </Card>);
           })}
       </div>
